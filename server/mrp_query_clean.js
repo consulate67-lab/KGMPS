fd7c0cb7526d44303cc2dd8e8560a959f8bf8764
Selim Korgun <selimkorgun@example.com>
2026-03-26 22:46:21 +0300

    fix: use original s_gchar logic for stock and purchases in MRP

--- server/mrp_query_clean.js
+++ server/mrp_query_clean.js
@@ -17,7 +17,7 @@ const getMrpQuery = (prodLocs, rawLocs) => {
     INSERT INTO @Orders
     SELECT 
         sh.SipNo, sh.SipHarinx, sh.SKod, 
-        (ISNULL(si.Giren, 0) - ISNULL(si.Cikan, 0)) as Miktar,
+        SUM(ISNULL(si.Giren, 0) - ISNULL(si.Cikan, 0)) as Miktar,
         RTRIM(sk.Location),
         ISNULL(upm.ModelKod, (SELECT TOP 1 c.ModelKod FROM stokmodel_c c WHERE c.SKod = sh.SKod ORDER BY c.insDT DESC))
     FROM siparis_kay sk
@@ -27,47 +27,48 @@ const getMrpQuery = (prodLocs, rawLocs) => {
     WHERE RTRIM(sk.SipTip) = 'S' 
       AND (RTRIM(sh.Durum) = '' OR sh.Durum IS NULL)
       AND RTRIM(sk.Location) IN (${pLocs})
-      AND (ISNULL(si.Giren, 0) - ISNULL(si.Cikan, 0)) > 0;
+    GROUP BY sh.SipNo, sh.SipHarinx, sh.SKod, sk.Location, upm.ModelKod
+    HAVING SUM(ISNULL(si.Giren, 0) - ISNULL(si.Cikan, 0)) > 0;
 
-    -- 2. HAMMADDE MEVCUT STOK (Fiziksel)
-    DECLARE @StokBak TABLE (SKod VARCHAR(30), Mevcut FLOAT);
+    -- 2. HAMMADDE MEVCUT STOK (Orijinal s_gchar mantığı)
+    DECLARE @StokBak TABLE (SKod VARCHAR(30), RKod INT, BedKod INT, Mevcut FLOAT);
     INSERT INTO @StokBak
-    SELECT SKod, SUM(ISNULL(Giren, 0) - ISNULL(Cikan, 0)) 
-    FROM stok_bak 
-    WHERE RTRIM(Location) IN (${rLocs})
-    GROUP BY SKod;
+    SELECT SKod, RKod, BedKod, SUM(ISNULL(Giren, 0) - ISNULL(Cikan, 0)) 
+    FROM s_gchar 
+    WHERE Modul = 'X' AND FisNo = 0 AND FisHarInx = 0
+      AND RTRIM(Location) IN (${rLocs})
+    GROUP BY SKod, RKod, BedKod;
 
-    -- 3. BEKLEYEN SATIN ALMA (Yoldaki)
-    DECLARE @SatinAlma TABLE (SKod VARCHAR(30), Bekleyen FLOAT);
+    -- 3. BEKLEYEN SATIN ALMA (Orijinal s_gchar Modul='S' mantığı)
+    DECLARE @SatinAlma TABLE (SKod VARCHAR(30), RKod INT, BedKod INT, Bekleyen FLOAT);
     INSERT INTO @SatinAlma
-    SELECT sh.SKod, SUM(ISNULL(sh.Miktar, 0))
-    FROM siparis_kay sk 
-    JOIN siparis_har sh ON sk.SipNo = sh.SipNo
-    WHERE RTRIM(sk.SipTip) = 'A' 
-      AND (RTRIM(sh.Durum) = '' OR sh.Durum IS NULL)
-      AND RTRIM(sk.Location) IN (${rLocs})
-    GROUP BY sh.SKod;
+    SELECT SKod, RKod, BedKod, SUM(ISNULL(Giren, 0) - ISNULL(Cikan, 0))
+    FROM s_gchar 
+    WHERE Modul = 'S' 
+      AND RTRIM(Location) IN (${rLocs})
+    GROUP BY SKod, RKod, BedKod;
 
     -- 4. TAM MRP ANALİZİ
     SELECT 
         pd.SKod as hskod,
         st_ham.Tanim as HSKod_Tanim,
-        ISNULL(rn.Tanim, '-') as HRKod_Tanim, -- Renk Detayı
-        '-' as HBeden, -- Beden detayı şuan için boş
+        ISNULL(rn.Tanim, '-') as HRKod_Tanim,
+        ISNULL(bd.Beden, '-') as HBeden,
         RTRIM(pd.Birim) as HBirim,
-        SUM(o.Miktar * pd.Miktar) as Miktar1, -- Brüt İhtiyaç (Siparişten Gelen)
-        ISNULL(sb.Mevcut, 0) as Miktar2,       -- Mevcut Stok (StokBak)
-        ISNULL(sa.Bekleyen, 0) as Miktar3,     -- Satın Alma (Bekleyen A Siparişleri)
-        (SUM(o.Miktar * pd.Miktar) - (ISNULL(sb.Mevcut, 0) + ISNULL(sa.Bekleyen, 0))) as Miktar4, -- Net İhtiyaç
+        SUM(o.Miktar * pd.Miktar) as Miktar1, -- Brüt İhtiyaç
+        MAX(ISNULL(sb.Mevcut, 0)) as Miktar2,       -- Mevcut Stok
+        MAX(ISNULL(sa.Bekleyen, 0)) as Miktar3,     -- Bekleyen Satın Alma
+        (SUM(o.Miktar * pd.Miktar) - (MAX(ISNULL(sb.Mevcut, 0)) + MAX(ISNULL(sa.Bekleyen, 0)))) as Miktar4, -- Net İhtiyaç
         SUM(o.Miktar * pd.Miktar) as MiktarTop
     FROM @Orders o
     JOIN model_PD pd ON RTRIM(pd.ModelKod) = RTRIM(o.ModelKod)
     JOIN StokKart st_ham ON st_ham.SKod = pd.SKod
     LEFT JOIN Model_PDS2R s2r ON s2r.ModelKod = pd.ModelKod AND s2r.Proses = pd.Proses AND s2r.Parcainx = pd.Parcainx AND s2r.SKod = pd.SKod
     LEFT JOIN Dbo.P_RNK_Tip rn ON rn.Renk_kod = s2r.RKod
-    LEFT JOIN @StokBak sb ON sb.SKod = pd.SKod
-    LEFT JOIN @SatinAlma sa ON sa.SKod = pd.SKod
-    GROUP BY pd.SKod, st_ham.Tanim, rn.Tanim, pd.Birim, sb.Mevcut, sa.Bekleyen
+    LEFT JOIN Dbo.P_Beden_D bd ON bd.Bedinx = 0 -- Beden detayı şimdilik basit tutuldu
+    LEFT JOIN @StokBak sb ON sb.SKod = pd.SKod AND (sb.RKod = s2r.RKod OR s2r.RKod IS NULL)
+    LEFT JOIN @SatinAlma sa ON sa.SKod = pd.SKod AND (sa.RKod = s2r.RKod OR s2r.RKod IS NULL)
+    GROUP BY pd.SKod, st_ham.Tanim, rn.Tanim, bd.Beden, pd.Birim
     HAVING SUM(o.Miktar * pd.Miktar) > 0
     ORDER BY pd.SKod;
   `;
